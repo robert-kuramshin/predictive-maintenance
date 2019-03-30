a684181189ca82201af092ea4b780f2dc1421aad
Robert Kuramshin <robert@sensorscall.com>
2019-03-30 12:46:40 -0400

    changing analog pins

--- main/adc1_example_main.c
+++ main/adc1_example_main.c
@@ -19,7 +19,7 @@
 
 static esp_adc_cal_characteristics_t *adc_chars;
 static const adc1_channel_t channel1 = ADC1_CHANNEL_6;     //GPIO34 if ADC1, GPIO14 if ADC2
-static const adc1_channel_t channel2 = ADC1_CHANNEL_3;     //GPIO34 if ADC1, GPIO14 if ADC2
+static const adc1_channel_t channel2 = ADC1_CHANNEL_0;     //GPIO34 if ADC1, GPIO14 if ADC2
 static const adc_atten_t atten = ADC_ATTEN_DB_0;
 static const adc_unit_t unit = ADC_UNIT_1;
 
