4a13c1a676c1bbe5feee0d121297d0efe7cb75d0
Robert Kuramshin <robert@sensorscall.com>
2019-04-16 04:50:28 -0400

    adding separate configs for each adc channel

--- main/main.c
+++ main/main.c
@@ -17,10 +17,18 @@
 
 #define DEFAULT_VREF    1130        //Use adc2_vref_to_gpio() to obtain a better estimate
 
-static esp_adc_cal_characteristics_t *adc_chars;
-static const adc1_channel_t channel1 = ADC1_CHANNEL_6;     //GPIO34 if ADC1, GPIO14 if ADC2
-static const adc1_channel_t channel2 = ADC1_CHANNEL_0;     //GPIO34 if ADC1, GPIO14 if ADC2
-static const adc_atten_t atten = ADC_ATTEN_DB_2_5;//0-1.10v
+static esp_adc_cal_characteristics_t *adc_chars_inlet;
+static esp_adc_cal_characteristics_t *adc_chars_outlet;
+static esp_adc_cal_characteristics_t *adc_chars_current;
+
+static const adc1_channel_t inlet = ADC1_CHANNEL_6;
+static const adc1_channel_t outlet = ADC1_CHANNEL_0; 
+static const adc1_channel_t current = ADC1_CHANNEL_3;     
+
+static const adc_atten_t atten_inlet    =   ADC_ATTEN_DB_11;//0-3.9V
+static const adc_atten_t atten_outlet   =   ADC_ATTEN_DB_11; //0-3.9V
+static const adc_atten_t atten_current  =   ADC_ATTEN_DB_0; //0-1.1V
+
 static const adc_unit_t unit = ADC_UNIT_1;
 
 static lis3dh_sensor_t* sensor;
@@ -71,47 +79,38 @@ void read_data ()
 {
     lis3dh_float_data_t data;
 
-    uint32_t adc_reading1 = 0;
-    uint32_t adc_reading2 = 0;
-    uint32_t voltage1;
-    uint32_t voltage2;
-    float psi1;
-    float psi2;
+    uint32_t adc_reading1 = adc1_get_raw(inlet); //pressure 1
+    uint32_t adc_reading2 = adc1_get_raw(outlet); //presure 2
+    uint32_t adc_reading3 = adc1_get_raw(current); //presure 2
 
-    adc_reading1 = adc1_get_raw(channel1);
-    adc_reading2 = adc1_get_raw(channel2);
+    uint32_t voltage1 = esp_adc_cal_raw_to_voltage(adc_reading1, adc_chars_inlet);
+    uint32_t voltage2 = esp_adc_cal_raw_to_voltage(adc_reading2, adc_chars_outlet);
+    uint32_t voltage3 = esp_adc_cal_raw_to_voltage(adc_reading3, adc_chars_current);
 
-    voltage1 = esp_adc_cal_raw_to_voltage(adc_reading1, adc_chars);
-    voltage2 = esp_adc_cal_raw_to_voltage(adc_reading2, adc_chars);
-
-    psi1 = (voltage1 / 1000.0 - 0.515) / 4.0 * 60;
-    psi2 = (voltage2 / 1000.0 - 0.527) / 4.0 * 60;
+    float psi1 = (voltage1 / 1000.0 - 0.515) / 4.0 * 60;
+    float psi2 = (voltage2 / 1000.0 - 0.527) / 4.0 * 60;
+    float current = (voltage3 / 1000.0) * 20;
     
 
     if (lis3dh_new_data(sensor) &&
         lis3dh_get_float_data(sensor, &data))
     {
-        printf("%.3f,%+7.3f,%+7.3f,%+7.3f,%f,%f\n",
-               (double)sdk_system_get_time() * 1e-3, data.ax, data.ay, data.az, psi1, psi2);
+        printf("%.3f,%+7.3f,%+7.3f,%+7.3f,%.3f,%.3f,%.3f\n",
+               (double)sdk_system_get_time() * 1e-3, data.ax, data.ay, data.az, psi1, psi2,current);
     }
 }
-
-/*
- * In this example, user task fetches the sensor values every seconds.
- */
 
 void user_task_periodic(void *pvParameters)
 {
     while (1)
     {
-        // read sensor data
         read_data();
     }
 }
 
 /* -- main program ------------------------------------------------- */
 
-void user_init(void)
+void accel_init(void)
 {
     uart_set_baudrate(I2C_BUS,2000000);
     // Give the UART some time to settle
@@ -137,16 +136,6 @@ void user_init(void)
         // LAST STEP: Finally set scale and mode to start measurements
         lis3dh_set_scale(sensor, lis3dh_scale_2_g);
         lis3dh_set_mode (sensor, lis3dh_odr_5000, lis3dh_normal, true, true, true);
-
-        /** -- TASK CREATION PART --- */
-
-        // must be done last to avoid concurrency situations with the sensor
-        // configuration part
-
-        // create a user task that fetches data from sensor periodically
-        xTaskCreate(user_task_periodic, "user_task_periodic", TASK_STACK_DEPTH, NULL, 12, NULL);
-
-
     }
     else
         printf("Could not initialize LIS3DH sensor\n");
@@ -159,21 +148,30 @@ void app_main()
     //Configure ADC
     if (unit == ADC_UNIT_1) {
         adc1_config_width(ADC_WIDTH_BIT_12);
-        adc1_config_channel_atten(channel1, atten);
-        adc1_config_channel_atten(channel2, atten);
-        adc2_config_channel_atten(ADC2_CHANNEL_8, atten);
+        adc1_config_channel_atten(inlet, atten_inlet);
+        adc1_config_channel_atten(outlet, atten_outlet);
+        adc1_config_channel_atten(current, atten_current);
     }
 
     adc2_vref_to_gpio(27);
     //uint32_t vref = get_vref();
 
     //Characterize ADC
-    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
-    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+    adc_chars_inlet= calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    adc_chars_outlet= calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    adc_chars_current= calloc(1, sizeof(esp_adc_cal_characteristics_t));
+
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten_inlet, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars_inlet);
     print_char_val_type(val_type);
 
-    user_init();
+    val_type = esp_adc_cal_characterize(unit, atten_outlet, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars_outlet);
+    print_char_val_type(val_type);
 
-    printf("raw1,voltage1(mV),pressure1(psi),raw2,voltage2(mV),pressure2(psi)\n");
+    val_type = esp_adc_cal_characterize(unit, atten_current, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars_current);
+    print_char_val_type(val_type);
+
+    accel_init();
+
+    xTaskCreate(user_task_periodic, "user_task_periodic", TASK_STACK_DEPTH, NULL, 12, NULL);
 }
 
