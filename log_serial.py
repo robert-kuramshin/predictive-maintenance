fde7a344e451c4998e26682e029a8124f8ce10c2
Robert Kuramshin <robert@sensorscall.com>
2019-04-16 05:00:42 -0400

    cleanup

--- log_serial.py
+++ log_serial.py
@@ -10,33 +10,13 @@ ser = serial.Serial('/dev/ttyUSB0')
 ser.baudrate = 2000000
 ser.flushInput()
 
-# plot_window = 20
-# y_var = np.array(np.zeros([plot_window]))
-
-# plt.ion()
-# fig, ax = plt.subplots()
-# line, = ax.plot(y_var)
-
 while True:
     try:
         ser_bytes = ser.readline()
 
-
-        # try:
-            
-        # except:
-        #     continue
-
         print(ser_bytes)
         with open("test.csv","a") as f:
             f.write(ser_bytes)
-        # y_var = np.append(y_var,decoded_bytes)
-        # y_var = y_var[1:plot_window+1]
-        # line.set_ydata(y_var)
-        # ax.relim()
-        # ax.autoscale_view()
-        # fig.canvas.draw()
-        # fig.canvas.flush_events()
     except:
         print("Keyboard Interrupt")
         break
