c3948947c7b885f783276af7ce2e70a829bf1851
Robert Kuramshin <robert.kuramshin98@gmail.com>
2019-04-22 10:07:34 -0400

    should not be shuffling

--- vibration/process_data.py
+++ vibration/process_data.py
@@ -7,10 +7,6 @@ import matplotlib.animation as animation
 #TRAIN
 train_data = pd.read_csv("../data_2/dataset.csv")
 
-train_data = train_data.sample(frac=1)
-
-train_data = train_data.reset_index()
-
 #TEST
 test_data = pd.read_csv("../data_2/sweep_inlet.csv")
 
